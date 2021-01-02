01a21d6ddc2c6d47902a4b122a2d3966c44a7e7c
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-01-02 23:01:22 +0900

    Added scales

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
   const chartWidth = 800;
   const innerHeight = chartHeight - margin.top - margin.bottom;
   const innerWidth = chartWidth - margin.left - margin.right;
+  const xScale = d3.scaleTime().range([0, innerWidth]);
+  const yScale = d3.scaleLinear().range([innerHeight, 0]);
+  xScale.domain([d3.min(data, d => d.Year), d3.max(data, d => d.Year)]);
+  yScale.domain([d3.min(data, d => d.Time), d3.max(data, d => d.Time)]);
+  const chartContainer = d3.select('div').append('svg').attr("height", chartHeight).attr("width", chartWidth);
+  const chart = chartContainer.append('g').attr("transform", `translate(${margin.left},${margin.top})`);
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+  const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+  chart.append('g').call(xAxis).attr("id", "x-axis").attr("transform", `translate(0,${innerHeight})`);
+  chart.append('g').call(yAxis).attr("id", "y-axis").attr("transform", `translate(0,0)`);
   console.log(data)
 })();
