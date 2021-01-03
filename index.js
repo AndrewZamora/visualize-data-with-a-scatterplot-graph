506146c6c327ee9525ebc6e64dcb19abd02bfd53
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-01-03 19:16:20 +0900

    Changed scales back to time, centered graph with flex-box, and removed commas from years on the x-axis

--- index.js
+++ index.js
@@ -5,13 +5,14 @@
   const chartWidth = 800;
   const innerHeight = chartHeight - margin.top - margin.bottom;
   const innerWidth = chartWidth - margin.left - margin.right;
-  const xScale = d3.scaleLinear().range([0, innerWidth]);
-  const yScale = d3.scaleLinear().range([innerHeight, 0]);
+  const xScale = d3.scaleTime().range([0, innerWidth]);
+  const yScale = d3.scaleTime().range([innerHeight, 0]);
   xScale.domain([d3.min(data, d => d.Year), d3.max(data, d => d.Year)]);
   yScale.domain([d3.min(data, d => d.Seconds), d3.max(data, d => d.Seconds)]);
   const chartContainer = d3.select('div').append('svg').attr("height", chartHeight).attr("width", chartWidth);
   const chart = chartContainer.append('g').attr("transform", `translate(${margin.left},${margin.top})`);
-  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+  // TickSizeOuter removes last empty tick and tickFormat removes comma from years
+  const xAxis = d3.axisBottom(xScale).tickSizeOuter(0).tickFormat(d3.format(""));
   const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
   chart.append('g').call(xAxis).attr("id", "x-axis").attr("transform", `translate(0,${innerHeight})`);
   chart.append('g').call(yAxis).attr("id", "y-axis").attr("transform", `translate(0,0)`);
